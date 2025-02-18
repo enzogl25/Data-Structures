ae6e0763e009b2a5c3b6a624f0a7206941f1b7f9
Enzo <enzo.guerra@me.com>
2025-02-17 21:14:08 -0300

    Added Stack!

--- basic/stack.cpp
+++ basic/stack.cpp
@@ -2,70 +2,107 @@
 #include <vector>
 using namespace std;
 
-using namespace std;
-
+// (PT) Definindo um alias 'type' como 'int' para facilitar mudanças futuras
+// (EN) Defining an alias 'type' as 'int' for easier future modifications
 using type = int;
 
-class Stack{
-    private:
-    vector<type> elements;
+// (PT) Implementação da classe Stack (Pilha) usando um vetor
+// (EN) Implementation of the Stack class using a vector
+class Stack {
+private:
+    vector<type> elements; // (PT) Vetor para armazenar os elementos
+                          // (EN) Vector to store the elements
 
-    public:
-    void push(type value){
+public:
+    // (PT) Adiciona um elemento ao topo da pilha
+    // (EN) Adds an element to the top of the stack
+    void push(type value) {
         elements.push_back(value);
+        cout << "Pushed " << value << " Into the stack." << endl;
     }
-    void pop(){
+
+    // (PT) Remove o elemento do topo da pilha
+    // (EN) Removes the element from the top of the stack
+    void pop() {
         if (elements.empty()) {
             cout << "Stack is empty. Cannot pop." << endl;
             return;
         }
+        cout << "Popped " << elements.back() << " from the stack." << endl;
         elements.pop_back();
     }
-    int top(){
-        if(elements.empty()){
-            return -1;
+
+    // (PT) Retorna o elemento do topo sem removê-lo
+    // (EN) Returns the top element without removing it
+    type top() {
+        if (elements.empty()) {
+            cout << "Stack is empty. No top element." << endl;
+            return -1; // Retorna -1 se estiver vazia (pode ser melhor usar std::optional)
         }
-        else{
-            return elements.back();
-        }
+        return elements.back();
     }
-    int size(){
+
+    // (PT) Retorna o número de elementos da pilha
+    // (EN) Returns the number of elements in the stack
+    int size() {
         return elements.size();
     }
-    bool empty(){
+
+    // (PT) Verifica se a pilha está vazia
+    // (EN) Checks if the stack is empty
+    bool empty() {
         return elements.empty();
     }
 
-    void display(){
-        if(elements.empty()){
+    // (PT) Exibe todos os elementos da pilha
+    // (EN) Displays all elements in the stack
+    void display() {
+        if (elements.empty()) {
             cout << "No elements to display" << endl;
+            return;
         }
-        else{
-            for(int i = elements.size() - 1; i >= 0; i--){
-                cout << elements[i];
-                if(i != 0)cout << " ";
-                else cout << endl;
-            }
+
+        cout << "Stack (top to bottom): ";
+        for (int i = elements.size() - 1; i >= 0; i--) {
+            cout << elements[i];
+            if (i != 0) cout << " -> ";
         }
-        
+        cout << endl;
     }
 };
 
+// (PT) Função principal para testar a pilha
+// (EN) Main function to test the stack
 int main() {
     Stack stack;
+
+    // (PT) Testando push
+    // (EN) Testing push
     stack.push(10);
     stack.push(20);
     stack.push(30);
-    stack.pop();
 
+    // (PT) Testando pop
+    // (EN) Testing pop
+    stack.pop(); // Remove 30
+
+    // (PT) Exibindo os elementos
+    // (EN) Displaying elements
     stack.display();
 
+    // (PT) Mostrando o elemento do topo
+    // (EN) Showing the top element
     cout << "Top element: " << stack.top() << endl;
 
+    // (PT) Mostrando o tamanho
+    // (EN) Showing the size
     cout << "Stack size: " << stack.size() << endl;
 
-    stack.pop();
-    stack.pop();
-    stack.pop(); 
+    // (PT) Removendo todos os elementos
+    // (EN) Removing all elements
+    stack.pop(); // Remove 20
+    stack.pop(); // Remove 10
+    stack.pop(); // Pilha vazia, erro tratado
+
     return 0;
 }
